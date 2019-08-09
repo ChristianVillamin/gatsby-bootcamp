e9af14d5f29709882248183bf2409176a07cb465
Christian Villamin <ChristianVillamin31@gmail.com>
2019-08-09 19:37:21 +0800

    rendering contentful posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,28 +7,24 @@ import style from "./blog.module.scss"
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
     }
   `)
 
-  const blogs = data.allMarkdownRemark.edges.map((edge, i) => {
+  const blogs = data.allContentfulBlogPost.edges.map((edge, i) => {
     return (
       <li className={style.post} key={`${edge}${i}`}>
-        <Link to={`/blog/${edge.node.fields.slug}`}>
-          <h2>{edge.node.frontmatter.title}</h2>
-          <p>{edge.node.frontmatter.date}</p>
+        <Link to={`/blog/${edge.node.slug}`}>
+          <h2>{edge.node.title}</h2>
+          <p>{edge.node.publishedDate}</p>
         </Link>
       </li>
     )
@@ -38,9 +34,45 @@ const BlogPage = () => {
     <Layout>
       <h1>Blog</h1>
       <p>Posts will show up here later on.</p>
-      <ol className={style.posts}>{blogs}</ol>
+      <ol className={style.post}>{blogs}</ol>
     </Layout>
   )
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     allMarkdownRemark {
+  //       edges {
+  //         node {
+  //           frontmatter {
+  //             title
+  //             date
+  //           }
+  //           fields {
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+  // const blogs = data.allMarkdownRemark.edges.map((edge, i) => {
+  //   return (
+  //     <li className={style.post} key={`${edge}${i}`}>
+  //       <Link to={`/blog/${edge.node.fields.slug}`}>
+  //         <h2>{edge.node.frontmatter.title}</h2>
+  //         <p>{edge.node.frontmatter.date}</p>
+  //       </Link>
+  //     </li>
+  //   )
+  // })
+
+  // return (
+  //   <Layout>
+  //     <h1>Blog</h1>
+  //     <p>Posts will show up here later on.</p>
+  //     <ol className={style.posts}>{blogs}</ol>
+  //   </Layout>
+  // )
 }
 
 export default BlogPage
